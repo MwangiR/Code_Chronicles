6c14523127227cf6f91c046e150e44a3a97d428f
Renny <rennymwangi@gmail.com>
2023-10-11 00:39:20 +1030

    UI design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { Outlet } from 'react-router-dom';
+import { Layout } from 'antd';
 import Navbar from './components/navbar';
-import Footer from './components/footer';
+import BottomNav from './components/footer';
+import './App.css';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Footer />
-    </>
+    <Layout className='container'>
+      <Header>
+        <Navbar />
+      </Header>
+
+      <Content>
+        <section>
+          <Outlet />
+        </section>
+      </Content>
+
+      <Footer>
+        <BottomNav />
+      </Footer>
+    </Layout>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import About from './pages/About';
+import Profile from './pages/Profile';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import 'antd/dist/reset.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <About />,
+      },
+      {
+        path: '/Profile',
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
