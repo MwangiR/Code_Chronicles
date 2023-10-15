cf5535851b114e363f30dfc869e6647f474c71a2
Renny Mwangi <rennymwangi@gmail.com>
2023-10-15 20:23:21 +1030

    Populated Portfolio

--- src/components/PortfolioList.jsx
+++ src/components/PortfolioList.jsx
@@ -1,30 +1,37 @@
-import { Avatar, Card, Col } from 'antd';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card, Col, Typography } from 'antd';
+import { LinkOutlined, GithubOutlined } from '@ant-design/icons';
+const { Text } = Typography;
 const { Meta } = Card;
 
-export default function PortfolioList() {
+export default function PortfolioList(props) {
   return (
     <Col span={18}>
       <Card
         hoverable
-        cover={
-          <img
-            alt='example'
-            src='http://www.shadowsphotography.co/wp-content/uploads/2017/12/photography-01-800x400.jpg'
-          />
-        }
+        cover={<img alt='example' src='https://picsum.photos/800/400?grayscale' />}
         actions={[
-          <SettingOutlined key='setting' />,
-          <EditOutlined key='edit' />,
-          <EllipsisOutlined key='ellipsis' />,
+          <a href={props.gitURL} target='_blank' rel='noopener noreferrer' key='github'>
+            <GithubOutlined />
+          </a>,
+          <a href={props.liveURL} target='_blank' rel='noopener noreferrer' key='liveServer'>
+            <LinkOutlined />
+          </a>,
         ]}
       >
         {/* <img src={props.image} alt='' style={{ display: 'block' }} /> */}
         <Meta
           avatar={<Avatar src='https://xsgames.co/randomusers/avatar.php?g=pixel' />}
-          title='Card title'
-          description='This is the description'
+          title={props.title}
+          description='Project'
         />
+        <Text
+          type='secondary'
+          style={{
+            marginTop: '1%',
+          }}
+        >
+          {props.description}
+        </Text>
       </Card>
     </Col>
   );
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,5 @@
 import { Card, Col, Row } from 'antd';
 import PortfolioList from '../components/PortfolioList';
-// import { Typography } from 'antd';
-
-// const { Text, Title } = Typography;
 
 export default function Home() {
   return (
@@ -20,34 +17,58 @@ export default function Home() {
       <Row gutter={16}>
         <Col className='gutter-row' span={8}>
           <div>
-            <PortfolioList />
+            <PortfolioList
+              title='Advertir'
+              gitURL='https://github.com/MwangiR/Advertir.git'
+              description='Contact Tracing Java Application: Advertir is a travel companion that utilised contact tracing to notify you when you were in close contact '
+            />
           </div>
         </Col>
         <Col className='gutter-row' span={8}>
           <div>
-            <PortfolioList />
+            <PortfolioList
+              title='Spotify Gig Finder'
+              gitURL='https://github.com/MwangiR/Group1_Project.git'
+              description='The following application displays concerts that are near you with regards to your liked songs'
+            />
           </div>
         </Col>
         <Col className='gutter-row' span={8}>
           <div>
-            <PortfolioList />
+            <PortfolioList
+              title='ViewPoint'
+              gitURL='https://github.com/MwangiR/View_Point.git'
+              description='Viewpoint combines Movies, TV Shows and Game review all in one spot as well as provide top 10 in each genre'
+            />
           </div>
         </Col>
       </Row>
       <Row gutter={16} style={{ marginTop: '1%' }}>
         <Col className='gutter-row' span={8}>
           <div>
-            <PortfolioList />
+            <PortfolioList
+              title='PostCraft'
+              gitURL='https://github.com/MwangiR/PostCraft.git'
+              description='PostCraft is a blogging application built on handlebars to enable communities to share their thoughts'
+            />
           </div>
         </Col>
         <Col className='gutter-row' span={8}>
           <div>
-            <PortfolioList />
+            <PortfolioList
+              title='CommerceSync'
+              gitURL='https://github.com/MwangiR/CommerceSync.git'
+              description='CommerceSync is a E-Commerce backend application that utilizes MySQL (relational database)'
+            />
           </div>
         </Col>
         <Col className='gutter-row' span={8}>
           <div>
-            <PortfolioList />
+            <PortfolioList
+              title='LinkMate'
+              gitURL='https://github.com/MwangiR/LinkMate.git'
+              description='LinkMate is a Social Network API is a backend application that empowers social networking platforms to manage user profiles, posts, and interactions seamlessly. It utilises NoSQL(MongoDB)'
+            />
           </div>
         </Col>
       </Row>
