d86b2cfbb1bb39d97687aaf773c0518f95a95f65
Renny Mwangi <rennymwangi@gmail.com>
2023-10-15 18:54:02 +1030

    Major Update: redesign of all pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,45 +1,54 @@
-import { Row, Col, Typography } from 'antd';
+import { useState, useEffect } from 'react';
+import { Card, Row, Col, Typography, Avatar } from 'antd';
 const { Paragraph, Text } = Typography;
 
 export default function About() {
+  const [fadeIn, setFadeIn] = useState(false);
+  useEffect(() => {
+    setFadeIn(true);
+  }, []);
   return (
-    <div className='aboutContent'>
-      <Row justify='center'>
-        <Col span={20} flex={3} style={{ marginTop: '5%' }}>
-          <Row>
-            <Col span={18} push={6} style={{ padding: '1%' }}>
-              <Row>
-                <Col>
-                  <Typography.Title level={3}>
-                    <Text code>Discover</Text>
-                  </Typography.Title>
+    <Card
+      hoverable
+      style={{
+        width: '100%',
+        padding: '2%',
+        margin: '2% 2% 5% 2%',
+        opacity: fadeIn ? 1 : 0,
+        transition: 'opacity 1s ease-in',
+      }}
+      bodyStyle={{
+        padding: 0,
+      }}
+    >
+      <Row gutter={[16, 16]}>
+        <Col xs={{ span: 24 }} sm={{ span: 8 }}>
+          <Avatar className='profilePhoto' size={400} />
+        </Col>
+        <Col xs={{ span: 24 }} sm={{ span: 16 }}>
+          <Typography.Title level={3}>
+            <Text code>/Discover </Text>
+          </Typography.Title>
 
-                  <Typography.Title style={{ fontFamily: 'DM Mono, monospace' }}>
-                    Hello, I&apos;m a Software developer
-                  </Typography.Title>
-                  <Paragraph style={{ textAlign: 'justify', lineHeight: '30px' }}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime officiis
-                    explicabo corporis magni id esse voluptas eveniet! Magnam itaque pariatur at
-                    quae quam, dolores fugiat fugit provident veritatis eius ex? Lorem ipsum dolor
-                    sit amet consectetur adipisicing elit. Ipsam tempore exercitationem neque vero
-                    in delectus, commodi illum, sapiente minima adipisci rem provident, repellendus
-                    officia? Veritatis facere obcaecati est odio delectus? Rem unde saepe suscipit
-                    facere, quo non corporis libero ut ipsum dignissimos nihil facilis corrupti
-                    numquam harum illo quas exercitationem. Tempora laudantium pariatur nulla vitae
-                    eum quas quidem impedit explicabo! Tenetur, quidem, eveniet saepe eligendi
-                    repudiandae asperiores mollitia tempora aliquam ratione ullam accusamus eum
-                    nobis suscipit laboriosam iste exercitationem, alias ipsam neque veritatis?
-                    Vitae vel ex, officia sequi earum ducimus?
-                  </Paragraph>
-                </Col>
-              </Row>
-            </Col>
-            <Col span={6} pull={18}>
-              <section className='profilePhoto'></section>
-            </Col>
-          </Row>
+          <Typography.Title style={{ fontFamily: 'DM Mono, monospace' }}>
+            / Hello, I&apos;m a Software developer /
+          </Typography.Title>
+
+          <Paragraph style={{ textAlign: 'justify', lineHeight: '30px' }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime officiis explicabo
+            corporis magni id esse voluptas eveniet! Magnam itaque pariatur at quae quam, dolores
+            fugiat fugit provident veritatis eius ex? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Ipsam tempore exercitationem neque vero in delectus, commodi illum,
+            sapiente minima adipisci rem provident, repellendus officia? Veritatis facere obcaecati
+            est odio delectus? Rem unde saepe suscipit facere, quo non corporis libero ut ipsum
+            dignissimos nihil facilis corrupti numquam harum illo quas exercitationem. Tempora
+            laudantium pariatur nulla vitae eum quas quidem impedit explicabo! Tenetur, quidem,
+            eveniet saepe eligendi repudiandae asperiores mollitia tempora aliquam ratione ullam
+            accusamus eum nobis suscipit laboriosam iste exercitationem, alias ipsam neque
+            veritatis? Vitae vel ex, officia sequi earum ducimus?
+          </Paragraph>
         </Col>
       </Row>
-    </div>
+    </Card>
   );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,32 +1,56 @@
 import { Card, Col, Row } from 'antd';
 import PortfolioList from '../components/PortfolioList';
-import { Typography } from 'antd';
+// import { Typography } from 'antd';
 
-const { Title } = Typography;
+// const { Text, Title } = Typography;
 
 export default function Home() {
   return (
-    <Row>
-      <Col span={20} offset={2} style={{ marginTop: '1%', marginBottom: '10%' }}>
-        <Card>
-          <Row>
-            <Col offset={3} span={20}>
-              <Title level={3}>Portfolio</Title>
-              <Card>
-                <Row gutter={10}>
-                  <PortfolioList message={'Tent'} />
-                  <PortfolioList message={'Test'} />
-                </Row>
-
-                <Row gutter={10} style={{ marginTop: '1%' }}>
-                  <PortfolioList message={'Tent'} />
-                  <PortfolioList message={'Test'} />
-                </Row>
-              </Card>
-            </Col>
-          </Row>
-        </Card>
-      </Col>
-    </Row>
+    <Card
+      hoverable
+      style={{
+        width: '100%',
+        padding: '2%',
+        margin: '2% 2% 5% 2%',
+      }}
+      bodyStyle={{
+        padding: 0,
+      }}
+    >
+      <Row gutter={16}>
+        <Col className='gutter-row' span={8}>
+          <div>
+            <PortfolioList />
+          </div>
+        </Col>
+        <Col className='gutter-row' span={8}>
+          <div>
+            <PortfolioList />
+          </div>
+        </Col>
+        <Col className='gutter-row' span={8}>
+          <div>
+            <PortfolioList />
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={16} style={{ marginTop: '1%' }}>
+        <Col className='gutter-row' span={8}>
+          <div>
+            <PortfolioList />
+          </div>
+        </Col>
+        <Col className='gutter-row' span={8}>
+          <div>
+            <PortfolioList />
+          </div>
+        </Col>
+        <Col className='gutter-row' span={8}>
+          <div>
+            <PortfolioList />
+          </div>
+        </Col>
+      </Row>
+    </Card>
   );
 }
